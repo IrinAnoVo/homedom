e406f03f9a4f8740686c6c2f6a53bec5075e351e
IrinAnoVo <irinapavelviktor@gmail.com>
2024-12-19 01:49:49 +0100

    наконец-то закончила))

--- src/main/java/dom/Zadachi3.java
+++ src/main/java/dom/Zadachi3.java
@@ -81,65 +81,109 @@ public class Zadachi3 {
 
             // 25. Создание массива чисел с четными индексами.
 //            int[] array = {1, 2, 3, 4, 5, 6};
+//            int count = 0;
+//            for (int i = 0; i < array.length; i += 2) {
+//                count++;
+//            }
+//            int[] newArray = new int[count];
+//            int co = 0;
+//            for (int i = 0; i < array.length; i += 2) {
+//                newArray[co++] = array[i];
+//            }
+//            System.out.println(Arrays.toString(newArray));
 
 
             // 26. Создание массива чисел с нечетными индексами.
 //            int[] array = {1, 2, 3, 4, 5, 6};
+//            int count = 0;
+//            for (int i = 1; i < array.length; i += 2) {
+//                count++;
+//            }
+//            int[] newArray = new int[count];
+//            int co = 0;
+//            for (int i = 1; i < array.length; i += 2) {
+//                newArray[co++] = array[i];
+//            }
+//            System.out.println(Arrays.toString(newArray));
 
 
             // 27. Разделение массива строк на два: строки с четной и нечетной длиной.
 //            String[] array = {"apple", "banana", "cat", "dog", "elephant"};
+//            String[] evenLength = new String[0];
+//            String[] oddLength = new String[0];
+//            for (String s : array) {
+//                if (s.length() % 2 == 0) {
+//                    evenLength = Arrays.copyOf(evenLength, evenLength.length + 1);
+//                    evenLength[evenLength.length - 1] = s;
+//                } else {
+//                    oddLength = Arrays.copyOf(oddLength, oddLength.length + 1);
+//                    oddLength[oddLength.length - 1] = s;
+//                }
+//                System.out.println("Even length: " + Arrays.toString(evenLength));
+//                System.out.println("Odd length: " + Arrays.toString(oddLength));
+//            }
 
 
             // 28. Найти все строки, которые начинаются и заканчиваются одинаковой буквой.
 //            String[] array = {"apple", "banana", "level", "radar", "hello"};
+//            String firstLetter = array[0].substring(0, 1);
+//            for (String str : array) {
+//                if (!str.startsWith(firstLetter) && str.charAt(0) == str.charAt(str.length() - 1)) {
+//                    System.out.println(str);
+//                }
+//            }
 
 
             // 29. Создать массив чисел, которые являются квадратами элементов исходного массива.
 //            int[] array = {1, 2, 3, 4, 5};
+//            int[] squares = new int[array.length];
+//            for (int i = 0; i < array.length; i++) {
+//                squares [i] = array[i] * array[i];
+//                }
+//                System.out.println("Squares: " + Arrays.toString(squares));
 
 
             // 30. Создать массив строк, содержащих только уникальные символы.
-            String[] array = {"apple", "banana", "cat", "dog", "race"};
-        String allWords = array[0];
-
-        for (int i = 1; i < array.length; i++) {
-            allWords += array[i];
-        }
-
-        System.out.println("allWords = " + allWords);
-
-        boolean isUnique = true;
-        int countUniqueLetters = 0;
-        for (int i = 0; i < allWords.length(); i++) {
-            for (int j = 1; i >= j; j++) {
-                if (allWords.charAt(i) == allWords.charAt(i-j)) {
-                    isUnique = false;
-                    break;
-                }
-            }
-            if (isUnique) {
-                countUniqueLetters++;
-            }
-            isUnique = true;
-        }
-
-        String[] arrayUniqueLetters = new String[countUniqueLetters];
-        int k = 0;
-        for (int i = 0; i < allWords.length(); i++) {
-            isUnique = true;
-            for (int j = 1; i >= j; j++) {
-                if (allWords.charAt(i) == allWords.charAt(i-j)) {
-                    isUnique = false;
-                    break;
-                }
-            }
-            if (isUnique) {
-                arrayUniqueLetters[k] = String.valueOf(allWords.charAt(i));
-                k++;
-            }
-        }
-        System.out.println("arrayUniqueLetters = " + Arrays.toString(arrayUniqueLetters));
+//            String[] array = {"apple", "banana", "cat", "dog", "race"};
+//        String allWords = array[0];
+//
+//        for (int i = 1; i < array.length; i++) {
+//            allWords += array[i];
+//        }
+//
+//        System.out.println("allWords = " + allWords);
+//
+//        boolean isUnique = true;
+//        int countUniqueLetters = 0;
+//        for (int i = 0; i < allWords.length(); i++) {
+//            for (int j = 1; i >= j; j++) {
+//                if (allWords.charAt(i) == allWords.charAt(i-j)) {
+//                    isUnique = false;
+//                    break;
+//                }
+//            }
+//            if (isUnique) {
+//                countUniqueLetters++;
+//            }
+//            isUnique = true;
+//        }
+//
+//        String[] arrayUniqueLetters = new String[countUniqueLetters];
+//        int k = 0;
+//        for (int i = 0; i < allWords.length(); i++) {
+//            isUnique = true;
+//            for (int j = 1; i >= j; j++) {
+//                if (allWords.charAt(i) == allWords.charAt(i-j)) {
+//                    isUnique = false;
+//                    break;
+//                }
+//            }
+//            if (isUnique) {
+//                arrayUniqueLetters[k] = String.valueOf(allWords.charAt(i));
+//                k++;
+//            }
+//        }
+//        System.out.println("arrayUniqueLetters = " + Arrays.toString(arrayUniqueLetters));
     }
 }
 
